ce2637d07bf2fe16d6698d3f64ce487ccd0abc84
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-04-11 18:32:30 +0300

    решение без доп задания

--- gallery.js
+++ gallery.js
@@ -1,9 +1,13 @@
 import img from './gallery-items.js';
 
 const galleryEl = document.querySelector('.js-gallery');
+const lightboxEl = document.querySelector('.js-lightbox');
+const lightboxImageEl = document.querySelector('.lightbox__image');
+const closeButtonEl = document.querySelector('[data-action="close-lightbox"]');
+
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
 const imagesMarkup = createImagesMarkup(img);
-
 galleryEl.insertAdjacentHTML('afterbegin', imagesMarkup);
 
 function createImagesMarkup(img) {
@@ -27,19 +31,26 @@ function createImagesMarkup(img) {
     })
     .join(' ');
 }
-
-galleryEl.addEventListener('click', clickOnImage);
-
-// function clickOnImage(event) {
-//   const originalImg = event.target.dataset.source;
-// }
-
-// console.log(clickOnImage());
-// Разбей задание на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных и предоставленному шаблону. +
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 // Открытие модального окна по клику на элементе галереи.
 // Подмена значения атрибута src элемента img.lightbox__image.
+
+galleryEl.addEventListener('click', OnImageClick);
+
+function OnImageClick(e) {
+  e.preventDefault();
+  lightboxEl.classList.add('is-open');
+  lightboxImageEl.src = e.target.dataset.source;
+  lightboxImageEl.alt = e.target.alt;
+}
+
 // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
 // Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+closeButtonEl.addEventListener('click', OnCloseButtonClick);
+
+function OnCloseButtonClick() {
+  lightboxEl.classList.remove('is-open');
+  lightboxImageEl.src = '';
+  lightboxImageEl.alt = '';
+}
